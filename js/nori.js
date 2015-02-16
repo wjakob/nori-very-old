be78d7bda1b9bd52a0297537c0f6c863362026df
Wenzel Jakob <wenzel@inf.ethz.ch>
2015-02-16 16:35:38 +0100

    website draft

--- js/nori.js
+++ js/nori.js
@@ -1,11 +1,9 @@
 !function ($) {
-	$(function(){ window.prettyPrint && prettyPrint() });
-
 	$('a[href*=#]').on('click', function(event){     
 		$('html,body').animate({scrollTop:$(this.hash + "-header").offset().top}, 500);
 	});
 
-	$("#prelim-body").load("prelim.html");
+	$("#prelim-body").load("prelim.html", function() { prettyPrint(); });
 	$("#pa1-body").load("pa1.html");
 	$("#pa2-body").load("pa2.html");
 
